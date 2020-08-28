d581972fa87ef21a5d850b02bdc8d5477273f574
Link <linkv50@163.com>
2020-08-28 08:30:35 +0800

    new: 1. SparseArray - 2DArray相互转化 2. 完善 ArrayUtil

--- data-structure-algorithms/src/main/java/cn/link/common/ArrayUtil.java
+++ data-structure-algorithms/src/main/java/cn/link/common/ArrayUtil.java
@@ -1,5 +1,15 @@
 package cn.link.common;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 数组、集合工具类
  *
@@ -10,14 +20,147 @@ package cn.link.common;
 public class ArrayUtil {
 
     /**
-     * 打印二维数组
-     * @param arr
+     * 定义一个的稀疏数组
+     *
+     * @param totalRow 总行数
+     * @param totalCol 总列数
+     * @param validNum 有效元素个数
+     * @param details  记录元素位置和元素的集合 (字符串逗号分隔行、列、值)
+     * @return sparseArr
      */
-    public static void printSecondDimensionArray(int[][] arr) {
+    public static int[][] defineSparseArr(int totalRow, int totalCol, int validNum, List<String> details) {
+
+        if (totalRow == 0 || totalCol == 0 || validNum == 0 || CollectionUtils.isEmpty(details)) {
+            return null;
+        }
+
+        /*
+            行 : 二维数组的有效元素个数 + 1(多一行记录二维数组的总行、列和有效元素个数)
+            列 : 3 (记录二维数组的:行、列、值)
+        */
+        int[][] sparseArr = new int[validNum + 1][3];
+
+        //第一行
+        sparseArr[0][0] = totalRow;
+        sparseArr[0][1] = totalCol;
+        sparseArr[0][2] = validNum;
+
+        //从第二行开始遍历
+        for (int i = 1; i < sparseArr.length; i++) {
+
+            if (StringUtils.isBlank(details.get(i - 1))) {
+                continue;
+            }
+
+            String detail = details.get(i - 1);
+            List<String> splitDetailList = Arrays.asList(detail.split(","));
+            sparseArr[i][0] = Integer.parseInt(splitDetailList.get(0));
+            sparseArr[i][1] = Integer.parseInt(splitDetailList.get(1));
+            sparseArr[i][2] = Integer.parseInt(splitDetailList.get(2));
+
+        }
+
+        return sparseArr;
+
+    }
+
+    /**
+     * 二维数组转稀疏数组
+     *
+     * @param secondDimensionArr 二维数组
+     * @param invalidVal         无效元素
+     * @return 稀疏数组
+     */
+    public static int[][] convert2dArrToSparseArr(int[][] secondDimensionArr, int invalidVal) {
+
+        //记录二维数组的:行、列
+        int totalRow = secondDimensionArr.length;
+        int totalCol = secondDimensionArr[0].length;
+        //记录有效值个数
+        int validNum = 0;
+        //记录有效值的集合
+        List<String> details = new ArrayList<>();
+
+        //遍历二维数组，开始记录有效值
+        for (int i = 0; i < secondDimensionArr.length; i++) {
+
+            for (int j = 0; j < secondDimensionArr[i].length; j++) {
+
+                if (secondDimensionArr[i][j] != invalidVal) {
+
+                    //有效值个数++
+                    validNum++;
+                    //记录有效值的行、列、值
+                    details.add("" + i + "," + j + "," + secondDimensionArr[i][j]);
+
+                }
+
+            }
+
+        }
+
+        return defineSparseArr(totalRow, totalCol, validNum, details);
+
+    }
+
+    /**
+     * 稀疏数组转二维数组
+     *
+     * @param sparseArr  稀疏数组
+     * @param invalidVal 无效值
+     * @return 二维数组
+     */
+    public static int[][] convertSparseArrTo2dArr(int[][] sparseArr, int invalidVal) {
+
+        if (sparseArr == null || sparseArr.length <= 1) {
+            return null;
+        }
+
+        //通过稀疏数组第一行的元素，定义二维数组的行、列
+        int totalRow = sparseArr[0][0];
+        int totalCol = sparseArr[0][1];
+        int[][] twoDimensionArr = new int[totalRow][totalCol];
+
+        //从第二行开始遍历，给二维数组赋有效值
+        for (int i = 1; i < sparseArr.length; i++) {
+
+            int row = sparseArr[i][0];
+            int col = sparseArr[i][1];
+            int val = sparseArr[i][2];
+            twoDimensionArr[row][col] = val;
+
+        }
+
+        //二维数组其余元素赋无效值
+        for (int i = 0; i < twoDimensionArr.length; i++) {
+
+            for (int j = 0; j < twoDimensionArr[i].length; j++) {
+
+                //0 是O未初始化的默认值，即未赋值
+                if (twoDimensionArr[i][j] == 0) {
+                    twoDimensionArr[i][j] = invalidVal;
+                }
+
+            }
+
+        }
+
+        return twoDimensionArr;
+
+    }
+
+    /**
+     * 打印二维数组
+     *
+     * @param arr 二维数组
+     */
+    public static void printSecondDimensionArr(int[][] arr) {
 
         if (arr == null || arr.length == 0) {
             return;
         }
+
+        System.out.println("==========SecondDimensionArr==========");
 
         for (int[] innerArr : arr) {
 
@@ -30,6 +173,7 @@ public class ArrayUtil {
 
                 if (j == innerArr.length - 1) {
                     System.out.print("," + innerArr[j] + "]");
+                    continue;
                 }
 
                 System.out.print("," + innerArr[j]);
@@ -39,6 +183,22 @@ public class ArrayUtil {
             System.out.println();
 
         }
+
+    }
+
+    public static void writeArr2File(int[][] arr, String filepath){
+
+        try {
+
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filepath));
+
+            out.close();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
 
     }
 
--- data-structure-algorithms/src/main/java/cn/link/datastructure/array/SparseArray.java
+++ data-structure-algorithms/src/main/java/cn/link/datastructure/array/SparseArray.java
@@ -11,6 +11,9 @@ import java.util.Random;
  * <p>
  * 当一个数组中大部分元素为０，或者为同一个值的数组时，可以使用稀疏数组来保存该数组
  *
+ * 行 : 二维数组的有效元素个数 + 1(多一行记录二维数组的总行、列和有效元素个数)
+ * 列 : 3 (记录二维数组的:行、列、值)
+ *
  * @author Link
  * @version 1.0
  * @date 2020/8/26 16:43
@@ -23,14 +26,7 @@ public class SparseArray {
     public static final int[][] SECOND_DIMENSION_ARRAY = new int[10][10];
 
     /**
-     * 定义稀疏数组
-     * 行 : 二维数组的长度 + 1(多一行记录二维数组的总行、列和有效元素个数)
-     * 列 : 3 (记录二维数组的:行、列、值)
-     */
-    public static final int[][] SPARSE_ARRAY = new int[SECOND_DIMENSION_ARRAY.length + 1][3];
-
-    /**
-     * 定义无效值
+     * 定义无效值 (在五子棋盘中 0 表示没放子)
      */
     public static final int INVALID_VAL = 0;
 
@@ -38,41 +34,31 @@ public class SparseArray {
      * 二维 -> 稀疏
      */
     @Test
-    public void secondDimensionArray2SparseArray() {
+    public void secondDimensionArr2SparseArr() {
 
-        //1. 第一行数组，记录二维数组的:行、列、值
-        SPARSE_ARRAY[0][1] = SECOND_DIMENSION_ARRAY.length;
-        SPARSE_ARRAY[0][2] = SECOND_DIMENSION_ARRAY[0].length;
-        int validVal = 0;
-        //行数从 1 开始
-        int sparseArrayRow = 1;
+        int[][] sparseArr = ArrayUtil.convert2dArrToSparseArr(SECOND_DIMENSION_ARRAY, INVALID_VAL);
 
-        for (int i = 0; i < SECOND_DIMENSION_ARRAY.length; i++) {
-
-            for (int j = 0; j < SECOND_DIMENSION_ARRAY[i].length; j++) {
-
-                if (SECOND_DIMENSION_ARRAY[i][j] != INVALID_VAL) {
-                    //有效值个数++
-                    validVal++;
-                    //记录有效值的行、列、值
-                    SPARSE_ARRAY[sparseArrayRow][0] = i;
-                    SPARSE_ARRAY[sparseArrayRow][1] = j;
-                    SPARSE_ARRAY[sparseArrayRow][2] = SECOND_DIMENSION_ARRAY[i][j];
-                    sparseArrayRow++;
-                }
-
-            }
-
-        }
-
-        SPARSE_ARRAY[0][3] = validVal;
-
-        ArrayUtil.printSecondDimensionArray(SPARSE_ARRAY);
+        ArrayUtil.printSecondDimensionArr(sparseArr);
 
     }
 
     /**
-     * 初始化一个 2/3 概率值为 INVALID_VAL (无效值)的二维数组
+     * 稀疏 -> 二维
+     */
+    @Test
+    public void sparseArr2secondDimensionArr(){
+
+        int[][] sparseArr = ArrayUtil.convert2dArrToSparseArr(SECOND_DIMENSION_ARRAY, INVALID_VAL);
+        ArrayUtil.printSecondDimensionArr(sparseArr);
+
+        int[][] secondDimensionArr = ArrayUtil.convertSparseArrTo2dArr(sparseArr, INVALID_VAL);
+        ArrayUtil.printSecondDimensionArr(secondDimensionArr);
+
+
+    }
+
+    /**
+     * 初始化一个 3/4 概率值是 无效值 的二维数组
      */
     @Before
     public void initSecondDimensionArray() {
@@ -84,7 +70,7 @@ public class SparseArray {
             for (int j = 0; j < SECOND_DIMENSION_ARRAY[i].length; j++) {
 
                 //正常值
-                if (random.nextInt(2) == 0) {
+                if (random.nextInt(3) == 0) {
 
                     //随机给一个 1 ~ 10 的数字作为元素
                     SECOND_DIMENSION_ARRAY[i][j] = random.nextInt(9) + 1;
@@ -99,6 +85,8 @@ public class SparseArray {
             }
 
         }
+
+        ArrayUtil.printSecondDimensionArr(SECOND_DIMENSION_ARRAY);
 
     }
 
