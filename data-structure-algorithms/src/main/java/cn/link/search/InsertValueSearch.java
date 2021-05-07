3f03bf94fcce11045e33c0f50dd1d9788879d984
link <linkv50@163.com>
2021-05-07 20:31:44 +0800

    TODO: 插值查找

---
+++ data-structure-algorithms/src/main/java/cn/link/search/InsertValueSearch.java
@@ -0,0 +1,15 @@
+package cn.link.search;
+
+/**
+ * 插值查找
+ *
+ * 在分布较为均匀的有序数组中，查找效率会比较高
+ *
+ * 原理：找arr[low]和arr[high]之间的大概的位置
+ *
+ * @author Link50
+ * @version 1.0
+ * @date 2021/5/7 20:27
+ */
+public class InsertValueSearch {
+}
--- data-structure-algorithms/src/main/java/cn/link/search/SearchTest.java
+++ data-structure-algorithms/src/main/java/cn/link/search/SearchTest.java
@@ -15,12 +15,12 @@ public class SearchTest {
     @Test
     public void testSearch() {
 
-        int[] arr = new int[]{0, 1, 1, 1, 4, 5, 6, 7, 8, 9};
+        int[] arr = new int[]{0, 1, 1, 1, 4, 4, 6, 7, 8, 9};
         int i = SeqSearch.search(arr, 1);
         System.out.println(i);
         i = BinarySearch.search(arr, 0, arr.length - 1, 1);
         System.out.println(i);
-        List<Integer> searchAll = BinarySearch.searchAll(arr, 1);
+        List<Integer> searchAll = BinarySearch.searchAll(arr, 4);
         System.out.println(searchAll.toString());
 
     }
