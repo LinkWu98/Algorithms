6843f6e6bf730c61dbea5c40ae16e9a76a85b327
link <linkv50@163.com>
2021-05-11 17:32:01 +0800

    UPDATE: 斐波那契查找

--- data-structure-algorithms/src/main/java/cn/link/search/FibonacciSearch.java
+++ data-structure-algorithms/src/main/java/cn/link/search/FibonacciSearch.java
@@ -1,5 +1,7 @@
 package cn.link.search;
 
+import java.util.Arrays;
+
 /**
  * 斐波那契查找
  * <p>
@@ -25,57 +27,68 @@ public class FibonacciSearch {
     public static int[] initFibonacciArr(int length) {
 
         int[] arr = new int[length];
-        arr[0] = 0;
+        arr[0] = 1;
         arr[1] = 1;
 
         for (int i = 2; i < arr.length; i++) {
-
             arr[i] = arr[i - 1] + arr[i - 2];
-
         }
 
         return arr;
 
     }
 
+    /**
+     * 大致原理：
+     * 总长 : f[n] = f[n - 1] + f[n - 2]
+     * mid = f[n - 1] - 1(数组下标需要长度 - 1)
+     * 以此来计算mid
+     *
+     * @param arr
+     * @param searchNum
+     * @returnd
+     */
     public static int search(int[] arr, int searchNum) {
 
         //偷懒随便初始化一个指定长度的
         int[] f = initFibonacciArr(arr.length);
-        int totalLength = arr.length;
-        int fibIdx = 0;
-        while (totalLength > f[fibIdx]) {
-            fibIdx++;
+        int fIndex = 0;
+        while (arr.length > f[fIndex]) {
+            fIndex++;
+        }
+
+        //初始化一个长度一致的数组，原数组长度后的用最后一个填充，防止下标越界
+        int[] temp = Arrays.copyOf(arr, f[fIndex]);
+        for (int i = arr.length; i < temp.length; i++) {
+            temp[i] = arr[arr.length - 1];
         }
 
         int low = 0;
         int high = arr.length - 1;
+        while (low <= high) {
 
-        //mid = low + f(n - 1)
-        int mid = low + f[fibIdx - 1];
-        while (arr[mid] != searchNum) {
-
-            if (searchNum > arr[mid]) {
-                low = mid + 1;
-            } else {
+            //到边界时，没法分割了，low = mid
+            int mid = fIndex == 0 ? low : low + f[fIndex - 1] - 1;
+            if (temp[mid] > searchNum) {
+                //此时总长 = f[n-1], mid在 low + f[n-1-1] - 1
                 high = mid - 1;
+                fIndex--;
+            } else if (temp[mid] < searchNum) {
+                //此时总长 = f[n-2], mid在 low + f[n - 2 - 1] - 1
+                low = mid + 1;
+                fIndex -= 2;
+            } else {
+                if (mid <= high) {
+                    return mid;
+                } else {
+                    //虽然符合了，但是超过边界，返回对应值
+                    return arr.length - 1;
+                }
             }
-
-            if (low > high) {
-                return -1;
-            }
-
-            fibIdx = 0;
-            while ((high - low + 1) > f[fibIdx]) {
-                fibIdx++;
-            }
-            //mid = fibIdx == 0 ? low : low + f[fibIdx - 1] - 1; 初始化斐波那契数列
-            mid = low + f[fibIdx - 1];
 
         }
 
-        return mid;
+        return -1;
 
     }
-
 }
