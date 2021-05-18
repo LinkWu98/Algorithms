a02f5c45d87624d69d372f0fbd853b875dfe1928
link <linkv50@163.com>
2021-05-18 10:31:05 +0800

    TODO: 后序遍历

--- data-structure-algorithms/src/main/java/cn/link/data/structure/tree/Tree.java
+++ data-structure-algorithms/src/main/java/cn/link/data/structure/tree/Tree.java
@@ -44,6 +44,47 @@ public class Tree<T> {
     }
 
     /**
+     * 非递归后续遍历
+     *
+     * 思路：
+     * 1. 一直入栈左边节点，直到没有左边节点
+     * 2. 看栈顶元素是否有右节点，有就入栈右节点，重复 1
+     * 3. 没有就输出栈顶元素
+     *
+     */
+    public void postOrder() {
+
+        if (root == null) {
+            return;
+        }
+
+        Stack<Node<T>> stack = new Stack<>();
+        Node<T> temp = root;
+        Node<T> prev = null;
+        while (temp != null || stack.size() > 0) {
+
+            while (temp != null) {
+                stack.push(temp);
+                temp = temp.leftChild;
+            }
+
+            //后序不同于中序，栈顶元素不马上pop，peek出来找右边元素
+            temp = stack.peek();
+
+
+            if (temp != null && prev != temp.rightChild) {
+                temp = temp.rightChild;
+            } else {
+                Node<T> node = stack.pop();
+                prev = node;
+                System.out.println(node.data);
+            }
+
+        }
+
+    }
+
+    /**
      * 节点
      */
     class Node<T> {
