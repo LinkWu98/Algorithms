3b7f5a4dcf5198771c39ce7ca2fb64e32e73713c
link <linkv50@163.com>
2021-06-21 16:31:07 +0800

    UPDATE: BST删除节点

--- data-structure-algorithms/src/main/java/cn/link/data/structure/tree/BinarySortTree.java
+++ data-structure-algorithms/src/main/java/cn/link/data/structure/tree/BinarySortTree.java
@@ -2,10 +2,8 @@ package cn.link.data.structure.tree;
 
 import cn.link.data.structure.linkedlist.Stack;
 import lombok.Data;
-import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 二叉排序树 - BST (BinarySortTree / BinarySearchTree)
@@ -82,28 +80,80 @@ public class BinarySortTree {
      *                       temp.left/right = target.left/right; parent.left/right = temp
      *                       )
      *
+     * 有重复的数就歇菜了，删除的时候无限循环，可以考虑再加个插入时间条件去匹配（类似版本号）
+     *
      * @return true 删除成功
      */
     public boolean remove(int data) {
 
-        //用中序遍历来找节点删除
         if (root == null) {
             return false;
         }
 
-        BSTNode parent = null;
+        BSTNode parent;
+        //用中序遍历找target的父节点
         if ((parent = getParentNode(data)) == null) {
             return false;
         }
 
+        //获取目标节点
+        BSTNode target = null;
+        if (parent.childSideFlag) {
+            target = parent.left;
+        } else {
+            target = parent.right;
+        }
 
+        //三种情况
+        BSTNode rightOfTarget = target.right;
+        BSTNode leftOfTarget = target.left;
+        if (rightOfTarget != null && leftOfTarget != null) {
+
+            //1. 有两颗子树 ->
+            //方案一   右边子树的最小节点替换要删除的节点(叶子结点,因为左右都没有节点,可以直接赋值,也符合二叉树的规律)，删除原最小节点
+            //方案二   左边子树的最大节点替换要删除的节点，删除原最大节点
+            BSTNode tempSwapNode = rightOfTarget;
+            while (tempSwapNode.left != null) {
+                tempSwapNode = tempSwapNode.left;
+            }
+            //删除原来的节点，如果后删除会死循环
+            this.remove(tempSwapNode.data);
+            //再左右赋上值
+            tempSwapNode.left = target.left;
+            tempSwapNode.right = target.right;
+            this.setNodeByParent(parent, tempSwapNode);
+
+        } else if (rightOfTarget != null || leftOfTarget != null) {
+
+            //2. 有单个子树 -> 子树的根节点替换要删除的节点
+            BSTNode tempSwapNode = rightOfTarget == null ? leftOfTarget : rightOfTarget;
+            this.setNodeByParent(parent, tempSwapNode);
+
+        } else {
+            //3. 无子节点，直接赋空
+            this.setNodeByParent(parent, null);
+        }
 
         return false;
 
     }
 
     /**
-     * 中序获取查询节点的父节点
+     * 根据选边，替换子节点
+     */
+    private void setNodeByParent(BSTNode parent, BSTNode swapNode) {
+        if (parent.childSideFlag) {
+            parent.left = swapNode;
+        } else {
+            parent.right = swapNode;
+        }
+    }
+
+    /**
+     * 思路一 ：中序获取查询想查找节点的父节点
+     * 思路二 ：根节点开始比较（充分运用树的性质）
+     *
+     * 此处采用思路一
      */
     public BSTNode getParentNode(int data) {
 
@@ -119,13 +169,16 @@ public class BinarySortTree {
             temp = stack.pop();
             if (temp.left != null) {
                 if (temp.left.data == data) {
-                    //TODO 左右标记
+                    //父节点标记要删除的子节点的边 true 左， false 右
+                    temp.childSideFlag = true;
                     return temp;
                 }
             }
 
             if (temp.right != null) {
                 if (temp.right.data == data) {
+                    //父节点标记要删除的子节点的边 true 左， false 右
+                    temp.childSideFlag = false;
                     return temp;
                 }
             }
@@ -175,7 +228,6 @@ public class BinarySortTree {
 }
 
 @Data
-@ToString
 class BSTNode {
 
     public BSTNode left;
@@ -183,8 +235,22 @@ class BSTNode {
     public BSTNode right;
 
     /**
+     * 用于删除时，父节点标记要删除的子节点的边 true 左， false 右
+     */
+    public boolean childSideFlag;
+
+    /**
      * 权
      */
     public int data;
 
+    @Override
+    public String toString() {
+        return "{" +
+                "data=" + data +
+                ", left=" + (left == null ? "null" : left.data) +
+                ", right=" + (right == null ? "null" : right.data) +
+                ", childSideFlag=" + childSideFlag +
+                '}';
+    }
 }
--- data-structure-algorithms/src/main/java/cn/link/data/structure/tree/TreeTest.java
+++ data-structure-algorithms/src/main/java/cn/link/data/structure/tree/TreeTest.java
@@ -51,14 +51,18 @@ public class TreeTest {
     public void testBST(){
 
         BinarySortTree bst = new BinarySortTree();
-        bst.add(1);
-        bst.add(4);
-        bst.add(2);
-        bst.add(3);
         bst.add(9);
-
+        bst.add(5);
+        bst.add(3);
+        bst.add(6);
+        bst.add(8);
+        bst.add(7);
         bst.infixTraversal();
 
+        bst.remove(5);
+        bst.infixTraversal();
+        bst.remove(7);
+        bst.infixTraversal();
     }
 
 }
