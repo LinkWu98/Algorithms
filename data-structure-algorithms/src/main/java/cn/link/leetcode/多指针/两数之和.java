9c01292b1b43412c247f3e2d5d4b9f464e03cbe5
伍霖康 <linkang.wu@volvocars.com>
2023-12-26 14:57:41 +0800

    两数之和 单指针简单解法

--- data-structure-algorithms/src/main/java/cn/link/leetcode/多指针/两数之和.java
+++ data-structure-algorithms/src/main/java/cn/link/leetcode/多指针/两数之和.java
@@ -1,5 +1,6 @@
 package cn.link.leetcode.多指针;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,5 +34,31 @@ public class 两数之和 {
         }
 
         return new int[2];
+    }
+
+    /**
+     * 单指针简单解法
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        int[] returnArr = new int[2];
+        Map<Integer, Integer> numIdxMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int currentNum = nums[i];
+            int findNum = target - currentNum;
+            Integer findIdx = numIdxMap.get(findNum);
+            if (findIdx == null){
+                numIdxMap.put(currentNum, i);
+                continue;
+            }
+            returnArr[0] = i;
+            returnArr[1] = findIdx;
+            break;
+        }
+        return returnArr;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,7,11,15};
+        System.out.println(Arrays.toString(twoSum2(nums, 9)));
     }
 }
